4303f8c43ede43d05a85a20a505a454a8383b925
Member1 <dewduniweerasundara7@gmail.com>
2026-04-23 21:27:41 +0530

    Add missing annotation in ResourceResponse.java

--- backend/src/main/java/com/sliit/smartcampus/repository/CampusResourceRepository.java
+++ backend/src/main/java/com/sliit/smartcampus/repository/CampusResourceRepository.java
@@ -6,4 +6,4 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 public interface CampusResourceRepository extends MongoRepository<CampusResource, String> {
 }
 
-// pass 5
+// pass 6
